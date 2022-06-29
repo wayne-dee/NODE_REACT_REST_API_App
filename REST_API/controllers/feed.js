80acbf28f56a9041e107d53f62cfdbba4d22b48d
Douglas <douglasonkeo3@gmail.com>
2022-06-29 23:27:31 +0300

    adding create post endpoint

--- REST_API/controllers/feed.js
+++ REST_API/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator')
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [ 
@@ -17,6 +19,10 @@ exports.getPosts = (req, res, next) => {
 // 
 // feed/post
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ message: "Validation failed"})
+    }
     const title = req.body.title;
     const content = req.body.content;
     // craete post in db
--- REST_API/routes/feed.js
+++ REST_API/routes/feed.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const { body } = require('express-validator')
 
 const feedControllers = require('../controllers/feed')
+
+const express = require('express');
 
 const router = express.Router();
 
 // GET /feed/posts
 router.get('/posts', feedControllers.getPosts);
 
-router.post('/post', feedControllers.createPost);
+router.post('/post', [
+    body('title').
+        trim()
+        .isLength({min: 5}),
+    body('content')
+        .trim()
+        .isLength({min: 5})
+], feedControllers.createPost);
 
 module.exports = router;
