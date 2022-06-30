50d1fae978d1e1181cd30bc6dcd0edc4cbf29da7
Douglas <douglasonkeo3@gmail.com>
2022-06-30 15:47:55 +0300

    adding user signup validation

--- REST_API/routes/auth.js
+++ REST_API/routes/auth.js
@@ -1,8 +1,30 @@
+const { body } = require('express-validator')
 
 const express = require('express');
+const user = require('../models/user');
+const authControllers = require('../controllers/auth')
 
 const router = express.Router();
 
-router.put('/signup');
+router.put('/signup',[
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value, { req }) => {
+            return user,findOne({email : value }).then(userDoc => {
+                if(userDoc) {
+                    return Promise.reject('E-mail already exists')
+                }
+            })
+        })
+        .normalizeEmail(),
+    body('password')
+        .trim()
+        .isLength({min: 5}),
+    body('name')
+        .trim()
+        .not()
+        .isEmpty()
+], );
 
 module.exports = router;
