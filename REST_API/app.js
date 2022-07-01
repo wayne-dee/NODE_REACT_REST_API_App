45fd9c5a0e6ebc90edbea3f8946d99c817f51bd6
Douglas <douglasonkeo3@gmail.com>
2022-07-01 13:38:50 +0300

    sharing sokect.IO instance across files

--- REST_API/app.js
+++ REST_API/app.js
@@ -65,7 +65,7 @@ mongoose.connect(
     const server = app.listen(8080)
     
     // connecting sockect io
-    const io = require('socket.io')(server);
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected')
     })
